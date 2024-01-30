66426d18288a451183deff59453f4c6b5ab57b69
slborisova <slborisova@gmail.com>
2024-01-29 21:25:08 -0500

    expected_conditions homework update

--- cypress/e2e/expected_conditions.cy.ts
+++ cypress/e2e/expected_conditions.cy.ts
@@ -1,17 +1,23 @@
-describe("EXPECTED CONDITIONS", () => {
+import { should } from "chai";
 
+describe("EXPECTED CONDITIONS", () => {
+  beforeEach(() => {
+    cy.visit(Cypress.env("expected_conditions"));
+  });
+  
+ 
     // Alert window?
     // it('Wait for alert to be present', () => {
     //     cy.visit(Cypress.env("expected_conditions"));
     //     cy.get('[id="alert_trigger"]').should("be.visible").and("exist").click()
+
     //     cy.get('[id="prompt_trigger"]').should("be.visible").and("exist").click()
 
         
     //  });
 
   it("Wait for element to be visible", () => {
-    cy.visit(Cypress.env("expected_conditions"));
-    cy.get('[id="visibility_trigger"]')
+      cy.get('[id="visibility_trigger"]')
       .should("be.visible")
       .and("exist")
       .click();
@@ -22,7 +28,6 @@ describe("EXPECTED CONDITIONS", () => {
   });
 
   it("Wait for element to be Invisible", () => {
-    cy.visit(Cypress.env("expected_conditions"));
     cy.get('[id="invisibility_target"]').should("be.visible").and("exist");
     cy.get('[id="invisibility_trigger"]')
       .should("be.visible")
@@ -32,27 +37,23 @@ describe("EXPECTED CONDITIONS", () => {
    });
 
  it('Wait for element to be enabled', () => {
-     cy.visit(Cypress.env("expected_conditions"));
-     cy.get('[id="enabled_trigger"]').should("be.visible").and("exist").click()
-     cy.get('[id="enabled_target"]').should("have.text", 'Enabled Button').click()
-     //cy.contains('button', 'Enabled Button')
-     cy.contains('.popover-header', 'Yay! I am super active now!')
-     cy.contains('.popover-body', 'See, you just clicked me!!')
+    cy.get('[id="enabled_target"]').should("be.visible").and("exist").contains("button","Disabled Button")
+    cy.get('[id="enabled_trigger"]').should("be.visible").and("exist").click()
+    cy.get('[id="enabled_target"]').should("have.text", 'Enabled Button').click()
+    cy.contains('.popover-header', 'Yay! I am super active now!')
+    cy.contains('.popover-body', 'See, you just clicked me!!')
   });
 
-//Bug?
-//   it('Wait for Page Title to change', () => {
-//     cy.visit(Cypress.env("expected_conditions"));
-//     cy.get('id="page_title_trigger"').should("be.visible").and("exist").click()
-//     //cy.contains('button', 'Enabled Button')
-//     cy.get('[id="page_title_trigger"]').should("have.text", 'My New Title')//selector should be changed
-//  });
+
+  it('Wait for Page Title to change', () => {
+    cy.get('[id="page_title_trigger"]').should("be.visible").and("exist").click()
+    cy.title().should("eq", "My New Title!");
+ });
 
 it('Wait for text/value to have specific values', () => {
-    cy.visit(Cypress.env("expected_conditions"));
-    cy.get('[id="text_value_trigger"]').should("be.visible").and("exist").click()
     cy.get('[class="spinner-grow spinner-grow-sm"]').should("be.visible").and("exist")
-    cy.contains('button', 'Submit')
+    cy.get('[id="text_value_trigger"]').should("be.visible").and("exist").click()
+    cy.get('[id="wait_for_text"]').contains('button', 'Submit')
  });
 
 // ??? iframe
